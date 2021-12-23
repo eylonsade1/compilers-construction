7d7baf395017d17a78d41d1912ff0e4ef02aa831
galbrow <galbrow@post.bgu.ac.il>
2021-12-23 18:26:13 -0500

    added working meir tester with box

--- semantic-analyser.ml
+++ semantic-analyser.ml
@@ -29,7 +29,19 @@ type expr' =
   | ScmLambdaOpt' of string list * string * expr'
   | ScmApplic' of expr' * (expr' list)
   | ScmApplicTP' of expr' * (expr' list);;
-
+  
+  let remove_elt e l =
+    let rec go l acc = match l with
+      | [] -> List.rev acc
+      | x::xs when e = x -> go xs acc
+      | x::xs -> go xs (x::acc)
+    in go l [];;
+  
+  let remove_duplicates l =
+    let rec go l acc = match l with
+      | [] -> List.rev acc
+      | x :: xs -> go (remove_elt x xs) (x::acc)
+    in go l [];;
 
 let var_eq v1 v2 =
 match v1, v2 with
@@ -124,7 +136,6 @@ module Semantic_Analysis : SEMANTIC_ANALYSIS = struct
    run pe [] [];;
 
   (* [1,2,3]  -> ([1,2],3) *)   
-
   let rec rdc_rac s =
     match s with
     | [e] -> ([], e)
@@ -158,30 +169,104 @@ module Semantic_Analysis : SEMANTIC_ANALYSIS = struct
     match tup with
       |(lst,item)-> (List.append (List.map (fun exp -> (run exp false)) lst) [(run item in_tail)])
        in
-   run pe false
+   run pe false;;
 
 
   (* boxing *)
-  let rec find_reads name enclosing_lambda expr =
-    match expr with
-    | ScmConst'(ScmVoid) -> _
-    | ScmSeq'(exprList) -> (List.map box_set exprList)
-    | ScmConst'(exp) ->
+  (*let find_reads name enclosing_lambda expr = *)
+  
+
+  (* function that checks if there is Read/Write as parameter*)
+  let checkIfVar name isRead isInner= 
+    let rec runCheck isBound env expr = 
+      (match expr with
+     | ScmVar'(VarBound(str,minor,major)) -> if isInner then if isRead then if (str = name) then [env] else [] else [] else []
+     | ScmVar'(VarParam(str,integer)) -> if isRead then if (str = name) then [env] else [] else []
+     | ScmIf'(expr1,expr2,expr3) -> (runCheck isBound env expr1)@((runCheck isBound env expr2)@(runCheck isBound env expr3))
+     | ScmSeq'(exprList) -> (List.fold_right List.append (List.map (fun exp -> (runCheck isBound env exp)) exprList) [])
+     | ScmSet'(var', expr') -> if isRead then (runCheck isBound env expr') else 
+          (match var' with
+            VarBound(str,minor,major) -> if isInner then if (str = name) then [env] else [] else []
+            |VarParam(str,integer) -> if isInner then [] else if (str = name) then [env] else []
+            |_ -> [])
+     | ScmDef'(var', expr') -> (runCheck isBound env expr')
+     | ScmOr'(exprList) -> (List.fold_right List.append (List.map (fun exp -> (runCheck isBound env exp)) exprList) [])
+     | ScmLambdaSimple'(stringList, expr') -> if (isInner && (not (List.mem name stringList))) then if isBound then (runCheck isBound env expr') else (runCheck true expr' expr') else []
+     | ScmLambdaOpt'(stringList, str, expr') -> if (isInner && (not (List.mem name stringList)) && str!=name) then if isBound then (runCheck isBound env expr') else (runCheck true expr' expr') else []
+     | ScmApplic'(expr', exprList) -> (List.append (runCheck isBound env expr') (List.fold_right List.append (List.map (fun exp -> (runCheck isBound env exp)) exprList) []))
+     | ScmApplicTP'(expr', exprList) -> (List.append (runCheck isBound env expr') (List.fold_right List.append (List.map (fun exp -> (runCheck isBound env exp)) exprList) []))
+     | _ -> []) in
+   runCheck;;
+
+   let replaceBox name = 
+    let rec runCheck expr = 
+      (match expr with
+     | ScmVar'(VarParam(str,minor)) ->  if (str = name) then ScmBoxGet'(VarParam(str,minor)) else expr
+     | ScmVar'(VarBound(str,minor,major)) ->  if (str = name) then ScmBoxGet'(VarBound(str,minor,major)) else expr
+     | ScmIf'(expr1,expr2,expr3) -> ScmIf'((runCheck expr1),(runCheck expr2),(runCheck expr3))
+     | ScmSeq'(exprList) -> ScmSeq'(List.map (fun exp -> (runCheck exp)) exprList)
+     | ScmSet'(var', expr') -> 
+      (match var' with
+        | VarParam(str,minor) ->  if (str = name) then ScmBoxSet'(VarParam(str,minor),(runCheck expr')) else ScmSet'(var',(runCheck expr'))
+        | VarBound(str,minor,major) ->  if (str = name) then ScmBoxSet'(VarBound(str,minor,major),(runCheck expr')) else ScmSet'(var',(runCheck expr'))
+        | VarFree(str) -> ScmSet'(var', (runCheck expr')))
+     | ScmDef'(var', expr') -> ScmDef'(var',(runCheck expr'))
+     | ScmOr'(exprList) -> ScmOr'(List.map (fun exp -> (runCheck exp)) exprList)
+     | ScmLambdaSimple'(stringList, expr') -> if (List.mem name stringList) then expr else ScmLambdaSimple'(stringList, (runCheck expr'))
+     | ScmLambdaOpt'(stringList, str, expr') -> if (List.mem name (str::stringList)) then expr else ScmLambdaOpt'(stringList, str, (runCheck expr'))
+     | ScmApplic'(expr', exprList) -> ScmApplic'((runCheck expr'), (List.map (fun exp -> (runCheck exp)) exprList)) 
+     | ScmApplicTP'(expr', exprList) -> ScmApplicTP'((runCheck expr'), (List.map (fun exp -> (runCheck exp)) exprList))
+     | exp -> exp) in
+   runCheck;;
+
+  let shouldBox env name body = 
+    let readerChecker = (checkIfVar name true false) in
+    let writerChecker = (checkIfVar name false false) in
+    let readExists = (readerChecker false body body) in
+    (*let a = Printf.printf "\n-----------------read-----------------------\n" in
+    let a = Printf.printf "%B" (readExists!=[]) in*)
+    let writeExists = (writerChecker false body body) in
+    (*let a = Printf.printf "\n-----------------write-----------------------\n" in
+    let a = Printf.printf "%B" (writeExists!=[]) in*)
+    let innerReads = ((checkIfVar name true true) false body body) in
+    (*let a = Printf.printf "\n-----------------inner read-----------------------\n" in
+    let a = Printf.printf "%B" (innerReads!=[]) in*)
+    let innerWrites = ((checkIfVar name false true) false body body) in
+    (*let a = Printf.printf "\n-----------------inner writes-----------------------\n" in
+    let a = Printf.printf "%B" (innerWrites!=[]) in *)
+    let unique_innerReads = (remove_duplicates innerReads) in
+    let unique_innerWrites = (remove_duplicates innerWrites) in
+    let differentClosures = (ormap (fun x -> (ormap (fun y->x!=y) unique_innerWrites)) unique_innerReads) in
+    (readExists!= [] && innerWrites != []) || (writeExists!=[] && innerReads != []) || differentClosures
+
+
+  let makeSet stringList lambda body = 
+    let shouldBoxArgs = (List.map (fun name -> (shouldBox lambda name body)) stringList) in
+    let index = -1 in
+    let makeParamBox name newBody flag = 
+      let index = index + 1 in (*check how to increment index*)
+      if flag then
+       match newBody with
+       | ScmSeq'(exprList) -> ScmSeq'(ScmSet'(VarParam(name, index),ScmBox'(VarParam(name,index)))::exprList)
+       | seq -> ScmSeq'(ScmSet'(VarParam(name, index),ScmBox'(VarParam(name,index)))::[seq]) else body in
+    let newBody = (List.fold_right2 (fun flag name body -> if flag then (replaceBox name body) else body) shouldBoxArgs stringList body) in
+    let newBody = (List.fold_right2 (fun flag name body -> (makeParamBox name body flag)) shouldBoxArgs stringList newBody) in
+    newBody;;  
     
 
-  let rec box_set expr = (*raise X_not_yet_implemented *)
-    function
+  let rec box_set expr =
+    match expr with
     | ScmConst'(sexpr) -> ScmConst'(sexpr)
     | ScmVar'(var') -> ScmVar'(var')
-    | ScmIf'(expr1, expr2, expr3) -> ScmIf'(expr1, expr2, expr3)
-    | ScmSeq'(exprList) -> ScmSeq'(exprList)
-    | ScmSet'(var', expr') -> ScmSet'(var', expr')
-    | ScmDef'(var', expr') -> ScmDef'(var', expr')
-    | ScmOr'(exprList) -> ScmOr'(exprList)
-    | ScmLambdaSimple'(stringList, expr') -> ScmLambdaSimple'(stringList, expr')
-    | ScmLambdaOpt'(stringList, str, expr') -> ScmLambdaOpt'(stringList, str, expr')
-    | ScmApplic'(expr', exprList) -> ScmApplic'(expr', exprList)
-    | ScmApplicTP'(expr', exprList) -> ScmApplicTP'(expr', exprList)
+    | ScmIf'(expr1, expr2, expr3) -> ScmIf'((box_set expr1), ( box_set expr2), (box_set expr3))
+    | ScmSeq'(exprList) -> ScmSeq'((List.map box_set exprList))
+    | ScmSet'(var', expr') -> ScmSet'(var', (box_set expr'))
+    | ScmDef'(var', expr') -> ScmDef'(var', (box_set expr'))
+    | ScmOr'(exprList) -> ScmOr'((List.map box_set exprList))
+    | ScmLambdaSimple'(stringList, expr') -> ScmLambdaSimple'(stringList, (makeSet stringList expr (box_set expr')))
+    | ScmLambdaOpt'(stringList, str, expr') -> ScmLambdaOpt'(stringList, str, (makeSet (str::stringList) expr (box_set expr')))
+    | ScmApplic'(expr', exprList) -> ScmApplic'((box_set expr'), (List.map box_set exprList))
+    | ScmApplicTP'(expr', exprList) -> ScmApplicTP'((box_set expr'), (List.map box_set exprList))
     | _ -> raise X_this_should_not_happen
 
   let run_semantics expr =
@@ -190,3 +275,9 @@ module Semantic_Analysis : SEMANTIC_ANALYSIS = struct
          (annotate_lexical_addresses expr))
 
 end;; (* end of module Semantic_Analysis *)
+
+
+
+(*
+
+*)
